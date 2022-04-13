df054c94ef6161413df54d83f811dc8fc8b13ee1
Juan Ramirez <juanmanuelramirez@users.noreply.github.com>
2022-04-13 13:42:30 -0500

    first commit

---
+++ CarController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Axle
+{
+    Front,
+    Back
+}
+
+public struct Wheel
+{
+    public GameObject model;
+    public WheelCollider wheelCollider;
+    public Axle axle;
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    [Header("References")]
+    public float motorInput;
+
+    [SerializeField]
+    private float maxAcceleration = 20f;
+    [SerializeField]
+    private float turnSensitive = 1.0f;
+    [SerializeField]
+    private float maxAngle = 45.0f;
+
+    private float inputX;
+
+    private Rigidbody _rb;
+
+    [Header("Input")]
+    public List<AxleInfo> axleInfos = new List<AxleInfo>();
+
+    public bool isAddingPositiveTorque;
+    public bool isAddingNegativeTorque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        GetInputs();
+        Move();
+    }
+
+    private void GetInputs()
+    {
+        if (isAddingPositiveTorque && !isAddingNegativeTorque)
+            motorInput = Mathf.Lerp(motorInput, 1, 2 * Time.deltaTime);
+        else if (!isAddingPositiveTorque && isAddingNegativeTorque)
+            motorInput = Mathf.Lerp(motorInput, -1, 2 * Time.deltaTime);
+        else if (isAddingPositiveTorque && isAddingNegativeTorque)
+            motorInput = Mathf.Lerp(motorInput, 0, 4 * Time.deltaTime);
+        else
+            motorInput = Mathf.Lerp(motorInput, 0, 4 * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        foreach (AxleInfo info in axleInfos)
+        {
+            if (info.isBack)
+            {
+                info.rigtWheel.motorTorque = motorInput * maxAcceleration * 500 * Time.deltaTime;
+                info.leftWheel.motorTorque = motorInput * maxAcceleration * 500 * Time.deltaTime;
+            }
+            if (info.isFront)
+            {
+                var _steerAngle = inputX * turnSensitive * maxAngle;
+                info.rigtWheel.steerAngle = Mathf.Lerp(info.rigtWheel.steerAngle, _steerAngle, 0.5f);
+                info.leftWheel.steerAngle = Mathf.Lerp(info.leftWheel.steerAngle, _steerAngle, 0.5f);
+            }
+
+            AnimateWheels(info.rigtWheel, info.visualRightWheel);
+            AnimateWheels(info.leftWheel, info.visualLeftWheel);
+        }
+    }
+
+    private void AnimateWheels(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Quaternion _rot;
+        Vector3 _pos;
+
+        Vector3 rotate = Vector3.zero;
+
+        wheelCollider.GetWorldPose(out _pos, out _rot);
+        wheelTransform.transform.rotation = _rot;
+        //wheelCollider.transform.position = _pos;
+    }
+
+    public void SetPositiveTorque(bool torque)
+    {
+        isAddingPositiveTorque = torque;
+    }
+    public void SetNegativeTorque(bool torque)
+    {
+        isAddingNegativeTorque = torque;
+    }
+}
+
+[System.Serializable]
+public class AxleInfo{
+    public WheelCollider rigtWheel;
+    public WheelCollider leftWheel;
+
+    public Transform visualRightWheel;
+    public Transform visualLeftWheel;
+
+    public bool isBack;
+    public bool isFront;
+}
---
+++ PlayMode.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMode : MonoBehaviour
+{
+    public CarController carController;
+
+    public bool isPositiveTorque;
+    private bool _isPositiveTorque;
+
+    public bool isNegativeTorque;
+    private bool _isNegativeTorque;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void ChangePositiveMotorTorque(bool value)
+    {
+        isPositiveTorque = value;
+    }
+    public void ChangeNegativeMotorTorque(bool value)
+    {
+        isNegativeTorque = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPositiveTorque != _isPositiveTorque)
+        {
+            carController.SetPositiveTorque(isPositiveTorque);
+            _isPositiveTorque = isPositiveTorque;
+        }
+        if (isNegativeTorque != _isNegativeTorque)
+        {
+            carController.SetNegativeTorque(isNegativeTorque);
+            _isNegativeTorque = isNegativeTorque;
+        }
+    }
+}
